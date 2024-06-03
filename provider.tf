d59e5e4a52bcc7eff6b873fa1cb5706e9e2bcdff
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-06-03 19:12:37 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
- subscription_id=var.subscription_id
-  client_id=var.client_id
-  client_secret=var.client_secret
-  tenant_id=var.tenant_id
