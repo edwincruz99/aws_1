d1892a2af596d9dfcdb229008f2d92b87875aa09
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-06-03 20:02:29 -0400

    Update versioning.tf

--- versioning.tf
+++ versioning.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
+  bucket = "test-bucket"
 }
 
 resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_test-bucket.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_test-bucket.id
   versioning_configuration {
     status = "Enabled"
   }
