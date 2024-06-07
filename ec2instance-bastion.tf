97311ae7a1390d9edb787c91ab26d6d310f7e552
edwincruz99 <168039723+edwincruz99@users.noreply.github.com>
2024-06-07 19:11:11 -0400

    Create ec2instance-bastion.tf

---
+++ ec2instance-bastion.tf
@@ -0,0 +1,16 @@
+module "ec2_public" {
+source = "terraform-aws-modules/ec2-instance/aws"
+#version = "2.17.0"
+version = "5.6.0"
+#variables
+name  = "${var.enviroment}-Bastionhost"
+# instance_count  = 5
+ami   = data.aws_ami.amzlinux2.id
+instance_type  = var.intance_type
+key_name = var.instance_keypair
+#monitoring  = true
+subnet_id  = module.vpc.public_subnet[0]
+#vpc_security_group_ids 
+vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+tag = local.comon_tags
+}
